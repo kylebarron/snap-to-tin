d84bafa7861ddcefeecb22ec1a9541bc2fe84cf3
Kyle Barron <barronk@mit.edu>
2020-03-12 16:42:33 -0600

    Keep `triangles` as typedarray

--- tests/rtree.test.js
+++ tests/rtree.test.js
@@ -1,4 +1,28 @@
-import { getNumLineSegments } from "../src/rtree";
+import { createTriangles, getNumLineSegments } from "../src/rtree";
+
+describe('createTriangles', () => {
+  test('create triangles correctly', () => {
+    const indices = Int32Array.from([
+      0, 1, 2, 2, 3, 0
+    ])
+    const positions = Float32Array.from([
+      0, 0, 10,
+      1, 1, 20,
+      0, 4, 30,
+      4, 0, 40,
+    ])
+    const result = createTriangles(indices, positions);
+    const expected = Float32Array.from([
+      0, 0, 10,
+      1, 1, 20,
+      0, 4, 30,
+      0, 4, 30,
+      4, 0, 40,
+      0, 0, 10,
+    ])
+    expect(result).toEqual(expected);
+  })
+})
 
 describe("getNumLineSegments", () => {
   test("break into 100 segments for diagonal line", () => {
